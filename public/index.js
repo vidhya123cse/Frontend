d5bb138622f0c63a8bb6d0d1b44edb8b41a0e940
Surejmohan <soorajmohan121997@gmail.com>
2020-03-15 20:14:05 +0530

    last

--- public/index.js
+++ public/index.js
@@ -40,18 +40,125 @@ border-color: #2dc997; border-radius: 7%; max-width: 950px;">
 					<div class="form-group col-sm-4 col-xs-12 col-sm-6 ">
 						<label><b>Email Address</b></label>
 						<input type="text" placeholder="Enter Email Address *" class="form-control">
-          </div>	
-        </div>
-						
-					<br><br><center><button type="button" class=" col-sm-2 col-sm-3 btn btn-lg btn-info"style=" background-color:#2dc997;">SIGNUP</button></center>	<br>				
+		  </div>	
+		  <br><br><br><br>
+		  <div class="col-sm-4 form-group col-xs-12 col-sm-6"style="border-color: #2dc997;border-radius: 7%;border-style: solid;"><br>
+		  <label><b>Instructions</b></label>
+		  <br>
+		  <ul>
+		  <li>Use this site for good and legal things only.Malpractises should be avoided.</li>
+		  <li>The input image you uploaded should  be clear. Noisy or incorrect image should lead to incorrect result. That willl not be our fault.</li>
+		  <li>All the information given should be correct and not illegaly created.</li>
+		  <li>Creating fake missing cases and use this site for search that person is a crime.</li>
+		  <li>Your personal information is completely encrypted and safe in our hand.</li>
+		  </ul>
+		  </div>
+								
+
+		</div>
+		
+	
+					<br><br><center><button type="button" id ="but3" class=" col-sm-2 col-sm-3 btn btn-lg btn-info" data-toggle="modal" data-target="#modalLRForm" style=" background-color:#2dc997;">SIGNUP</button></center>	<br>	
+
+	
+
+								
 					</div>
 	
 				</div>
     </div>
-    </div>
+	</div>
+	
     `
     
     document.getElementById("register").innerHTML= htm; 
     
 
 }
+
+
+function User()
+{
+
+
+htm = `
+<h2 class="well" style="min-width: 320px;margin-top:2%; text-align: center;font-weight: bold;"id ="register" >Registration Form</h2>
+<div class="col-lg-12 ">
+<div class=" col-xs-12 col-sm-6 col-md-9">
+				
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 form-group">
+							<label><b>First Name</b></label>
+							<input type="text" placeholder="Enter First Name *" class="form-control" value="" />
+						</div>
+						<div class="col-xs-12 col-sm-6 form-group">
+							<label><b>Last Name</b></label>
+							<input type="text" placeholder="Enter Last Name *" class="form-control" value="" />
+						</div>
+		</div>					
+		<div class="form-group col-xs-12 col-sm-6">
+							<label><b>Address</b></label>
+							<textarea placeholder="Enter Address Here.." rows="3" class="form-control"></textarea>
+						</div>	
+						<div class="row">
+							<div class="col-sm-4 form-group col-xs-12 col-sm-6 ">
+								<label><b>City</b></label>
+								<input type="text" placeholder="Enter City Name Here.." class="form-control">
+							</div>	
+							<div class="col-sm-4 form-group col-xs-12 col-sm-6 ">
+								<label><b>State</b></label>
+								<input type="text" placeholder="Enter State Name Here.." class="form-control">
+							</div>	
+							<div class="col-sm-4 form-group col-xs-12 col-sm-6 ">
+								<label><b>Zip</b></label>
+								<input type="text" placeholder="Enter Zip Code Here.." class="form-control">
+							</div>		
+						</div>
+						<button type="button" style="max-width: 50%;" class="btn btn-lg btn-info">Submit</button>	<br>
+						</div>
+						
+  </div> 
+						`
+
+document.getElementById("user").innerHTML= htm;
+
+}
+
+
+function Authority()
+{
+
+
+htm = `<div class="form-group col-sm-4 col-xs-12 col-sm-6 ">
+<label><b>Authority Address</b></label>
+<input type="text" placeholder="Enter Email Address *" class="form-control">
+</div>	 `
+
+document.getElementById("authority").innerHTML= htm;
+
+}
+
+$("#but1").click(function () {
+
+	$("#but2").hide();
+	$("#but1").hide();
+	$("#authority").hide();
+	$("#user").show();
+});
+
+$("#but2").click(function () {
+
+	$("#but1").hide();
+	$("#but2").hide();
+	$("#user").hide();
+	$("#authority").show();
+});
+
+
+$("#but4").click(function () {
+
+	$("#but1").show();
+	$("#but2").show();
+	$("#authority").hide();
+	$("#user").hide();
+});
