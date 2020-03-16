4e11431b52808ebee4373f1de31f22b73996e510
Surej Mohan <soorajmohan121997@gmail.com>
2020-03-17 01:05:14 +0530

    copyright

--- public/index.js
+++ public/index.js
@@ -22,7 +22,7 @@ border-color: #2dc997; border-radius: 7%; max-width: 950px;">
             <div class="row">
 							<div class="col-xs-12 col-sm-6 form-group">
 								<label><b>Password</b></label>
-								<input type="text" placeholder="Enter Password *" class="form-control">
+								<input type="password" placeholder="Enter Password *" class="form-control">
 							</div>		
 							<div class="col-sm-6 form-group">
 								<label><b>Confirm password</b></label>
@@ -34,25 +34,15 @@ border-color: #2dc997; border-radius: 7%; max-width: 950px;">
             <div class="row">					
 					<div class="col-sm-4 form-group col-xs-12 col-sm-6 ">
 						<label><b>Mobile Number</b></label>
-            <input type="text" placeholder="Enter Mobile Number *" class="form-control" value="">	</div>
+            <input type="tel" placeholder="Enter Mobile Number *" class="form-control" value="">	</div>
             
 				
 					<div class="form-group col-sm-4 col-xs-12 col-sm-6 ">
 						<label><b>Email Address</b></label>
 						<input type="text" placeholder="Enter Email Address *" class="form-control">
 		  </div>	
-		  <br><br><br><br>
-		  <div class="col-sm-4 form-group col-xs-12 col-sm-6"style="border-color: #2dc997;border-radius: 7%;border-style: solid;"><br>
-		  <label><b>Instructions</b></label>
-		  <br>
-		  <ul>
-		  <li>Use this site for good and legal things only.Malpractises should be avoided.</li>
-		  <li>The input image you uploaded should  be clear. Noisy or incorrect image should lead to incorrect result. That willl not be our fault.</li>
-		  <li>All the information given should be correct and not illegaly created.</li>
-		  <li>Creating fake missing cases and use this site for search that person is a crime.</li>
-		  <li>Your personal information is completely encrypted and safe in our hand.</li>
-		  </ul>
-		  </div>
+		  <br><br>
+		  
 								
 
 		</div>
@@ -113,11 +103,39 @@ htm = `
 								<label><b>Zip</b></label>
 								<input type="text" placeholder="Enter Zip Code Here.." class="form-control">
 							</div>		
+					
+
+						<div class="form-group"><br>
+						<label class=" control-label" for="filebutton"><b>Upload Any proof</b></label>
+						<div class="col-xs-12 col-sm-6 col-md-9">
+						  <input name="filebutton" class="input-file" id="filebutton" type="file">
 						</div>
+					  </div>
+					  </div>
+					  </div>
+					  </div> 	
+					<br>
+						<div class="col-sm-4 container-fluid col-xs-12 col-sm-6"style="border-color: #2dc997;margin:auto auto auto auto; min-width: 300px; max-width: 85%; border-radius: 7%;border-style: solid;"><br>
+		  <label><b style="font-size: 27px;">Instructions</b></label>
+		  <br>
+		  <ul>
+		  <li>Use this site for good and legal things only.Malpractises should be avoided.</li>
+		  <li>The input image you uploaded should  be clear. Noisy or incorrect image should lead to incorrect result. That willl not be our fault.</li>
+		  <li>All the information given should be correct and not illegaly created.</li>
+		  <li>Creating fake missing cases and use this site for search that person is a crime.</li>
+		  <li>Your personal information is completely encrypted and safe in our hand.</li>
+		  </ul>
+		<br>
+		<br>
+		<input type="checkbox" class="form-check-input" id="instruct" value="done">
+		<label class="form-check-label"> I have a Read the Instructions</label><br><br><br>
+		  </div> 
+
+		
 						<button type="button" style="max-width: 50%;" class="btn btn-lg btn-info">Submit</button>	<br>
-						</div>
 						
-  </div> 
+						
+  
 						`
 
 document.getElementById("user").innerHTML= htm;
@@ -125,14 +143,45 @@ document.getElementById("user").innerHTML= htm;
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 function Authority()
 {
 
 
-htm = `<div class="form-group col-sm-4 col-xs-12 col-sm-6 ">
+htm = `<div class="form-group col-sm-4 col-xs-12 col-sm-6"style ="margin:auto auto auto auto;">
 <label><b>Authority Address</b></label>
 <input type="text" placeholder="Enter Email Address *" class="form-control">
-</div>	 `
+</div>
+<div class="col-sm-4 form-group col-xs-12 col-sm-6"style="border-color: #2dc997;margin:auto auto auto auto; min-width: 300px; max-width: 85%; border-radius: 7%;border-style: solid;"><br>
+<label><b style="font-size: 27px;">Instructions</b></label>
+<br>
+<ul>
+<li>Use this site for good and legal things only.Malpractises should be avoided.</li>
+<li>The input image you uploaded should  be clear. Noisy or incorrect image should lead to incorrect result. That willl not be our fault.</li>
+<li>All the information given should be correct and not illegaly created.</li>
+<li>Creating fake missing cases and use this site for search that person is a crime.</li>
+<li>Your personal information is completely encrypted and safe in our hand.</li>
+</ul>
+<br>
+<br>
+<input type="checkbox" class="form-check-input" id="instruct" value="done">
+<label class="form-check-label"> I have a Read the Instructions</label><br><br><br>
+</div> 
+
+
+			  <button type="button" style="max-width: 50%;" class="btn btn-lg btn-info">Submit</button>	<br>
+	 `
 
 document.getElementById("authority").innerHTML= htm;
 
@@ -140,10 +189,11 @@ document.getElementById("authority").innerHTML= htm;
 
 $("#but1").click(function () {
 
-	$("#but2").hide();
 	$("#but1").hide();
+	$("#but2").hide();
 	$("#authority").hide();
 	$("#user").show();
+
 });
 
 $("#but2").click(function () {
@@ -152,6 +202,7 @@ $("#but2").click(function () {
 	$("#but2").hide();
 	$("#user").hide();
 	$("#authority").show();
+	
 });
 
 
@@ -161,4 +212,9 @@ $("#but4").click(function () {
 	$("#but2").show();
 	$("#authority").hide();
 	$("#user").hide();
+	
+
 });
+
+
+
